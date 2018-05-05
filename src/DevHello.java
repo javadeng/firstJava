e98e8c71f239aa60a3a9e09fbd6a356e71bcd4cb
v_dengjianghua <v_dengjianghua@baidu.com>
2018-05-05 12:57:39 +0800

    commit code on dev

---
+++ src/DevHello.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: v_dengjianghua
+ * Date: 2018/5/5
+ * Time: 12:56
+ * To change this template use File | Settings | File And Code Templates.
+ * Description:
+ */
+public class DevHello {
+
+    public void testDev() {
+        System.out.println("hello dev");
+    }
+
+}
