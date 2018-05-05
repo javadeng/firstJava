0fb9265fed59353cd50e489349a6bffb082ff664
v_dengjianghua <v_dengjianghua@baidu.com>
2018-05-05 12:54:36 +0800

    commit code

---
+++ src/HelloDeng.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: v_dengjianghua
+ * Date: 2018/5/5
+ * Time: 12:53
+ * To change this template use File | Settings | File And Code Templates.
+ * Description:
+ */
+public class HelloDeng {
+
+    public void test1() {
+        System.out.println("hello deng");
+    }
+
+}
