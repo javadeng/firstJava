da504bde8dbb4bbdf34c950c1d86af1d7c6f727f
v_dengjianghua <v_dengjianghua@baidu.com>
2018-05-05 13:21:20 +0800

    commit master

---
+++ src/MasterHello.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: v_dengjianghua
+ * Date: 2018/5/5
+ * Time: 13:20
+ * To change this template use File | Settings | File And Code Templates.
+ * Description:
+ */
+public class MasterHello {
+
+    public void test(){
+        System.out.println("hello master1");
+    }
+
+}
