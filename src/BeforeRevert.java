e0dc97f9dac405da16dfee700522cfcf88a0567d
v_dengjianghua <v_dengjianghua@baidu.com>
2018-05-05 13:23:37 +0800

    commit code on dev : after revert

---
+++ src/BeforeRevert.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: v_dengjianghua
+ * Date: 2018/5/5
+ * Time: 13:22
+ * To change this template use File | Settings | File And Code Templates.
+ * Description:
+ */
+public class BeforeRevert {
+
+    public void beforeRevert(){
+        System.out.println("hello before revert");
+    }
+
+}
